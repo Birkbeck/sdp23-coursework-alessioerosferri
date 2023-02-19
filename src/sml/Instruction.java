03c7a3e4c59f8de3627c783462c3e49bc6df3d26
Alessio Ferri <alessio.ferri@thoughtworks.com>
2023-02-19 18:03:04 +0000

    Completed TODOs in Instruction.java

--- src/sml/Instruction.java
+++ src/sml/Instruction.java
@@ -1,11 +1,8 @@
 package sml;
 
-// TODO: write a JavaDoc for the class
-
 /**
- * Represents an abstract instruction.
- *
- * @author ...
+ * Represents an abstract instruction in a Simple Machine Language (SML) program.
+ * @author alessioerosferri
  */
 public abstract class Instruction {
 	protected final String label;
@@ -13,20 +10,25 @@ public abstract class Instruction {
 
 	/**
 	 * Constructor: an instruction with a label and an opcode
-	 * (opcode must be an operation of the language)
 	 *
-	 * @param label optional label (can be null)
-	 * @param opcode operation name
+	 * @param label the label for the instruction (can be null)
+	 * @param opcode the opcode for the instruction (must be a valid operation of the language)
 	 */
 	public Instruction(String label, String opcode) {
 		this.label = label;
 		this.opcode = opcode;
 	}
 
+	/**
+	 * @return the label for this instruction (can be null)
+	 */
 	public String getLabel() {
 		return label;
 	}
 
+	/**
+	 * @return the opcode for this instruction
+	 */
 	public String getOpcode() {
 		return opcode;
 	}
@@ -34,24 +36,40 @@ public abstract class Instruction {
 	public static int NORMAL_PROGRAM_COUNTER_UPDATE = -1;
 
 	/**
-	 * Executes the instruction in the given machine.
+	 * Executes the instruction on the given machine.
 	 *
-	 * @param machine the machine the instruction runs on
-	 * @return the new program counter (for jump instructions)
-	 *          or NORMAL_PROGRAM_COUNTER_UPDATE to indicate that
-	 *          the instruction with the next address is to be executed
+	 * @param machine the machine to execute the instruction on
+	 * @return the new program counter value (for jump instructions),
+	 *         or NORMAL_PROGRAM_COUNTER_UPDATE to indicate that the next instruction should be executed
 	 */
-
 	public abstract int execute(Machine machine);
 
+	/**
+	 * @return the label string for this instruction (empty string if label is null)
+	 */
 	protected String getLabelString() {
 		return (getLabel() == null) ? "" : getLabel() + ": ";
 	}
 
-	// TODO: What does abstract in the declaration below mean?
-	//       (Write a short explanation.)
+	// Abstract for the following methods means that they cannot be implemented at this level, but all concrete subclasses
+	// must override these methods. The reason for this is that at this level of abstraction we need to leave to subclasses the
+	// implementation, as they will know best how to implement each of these methods.
+	/**
+	 * @return a string representation of this instruction (implementation left to concrete subclasses)
+	 */
 	@Override
 	public abstract String toString();
 
-	// TODO: Make sure that subclasses also implement equals and hashCode (needed in class Machine).
+	/**
+	 * @param o the object to compare to this instruction
+	 * @return true if the object is an instruction with the same label and opcode, false otherwise
+	 */
+	@Override
+	public abstract boolean equals(Object o);
+
+	/**
+	 * @return the hash code for this instruction (implementation left to concrete subclasses)
+	 */
+	@Override
+	public abstract int hashCode();
 }
