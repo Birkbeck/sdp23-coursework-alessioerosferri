84fc9d185b40692a40a8d84c5e59b583ffbc5280
Alessio Ferri <alessio.ferri@thoughtworks.com>
2023-02-25 13:42:12 +0000

    Refactored javadoc

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -6,12 +6,14 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
- * This class contains the labels used by the program. It allows the machine to reference instruction addresses by label (if a label is used).
+ * The Labels class contains the labels used by the program. It allows the machine to reference instruction addresses by label (if a label is used).
  * It ensures uniqueness of labels in the SML program.
  *
  * @author alessioerosferri
  */
 public final class Labels {
+
+	/** The map containing the labels and their associated addresses. */
 	private final Map<String, Integer> labels = new HashMap<>();
 
 	/**
@@ -19,7 +21,7 @@ public final class Labels {
 	 *
 	 * @param label the label
 	 * @param address the address the label refers to
-	 * @throws IllegalArgumentException if label already existing.
+	 * @throws IllegalArgumentException if label already exists.
 	 */
 	public void addLabel(String label, int address) {
 		Objects.requireNonNull(label);
@@ -35,13 +37,9 @@ public final class Labels {
 	 *
 	 * @param label the label
 	 * @return the address the label refers to
-	 * @throws NullPointerException if label already existing.
+	 * @throws NullPointerException if label does not have an associated address.
 	 */
 	public int getAddress(String label) {
-		// Where can NullPointerException be thrown here?
-		// Checking whether a label exists is something that could happen either in here or in the Translator class. Because both Translator and Labels class are public, and therefore could be used by any user (engineer)
-		// I am making a design decision to check non-existent labels in both places. Translator ensures the program text passed in can be successfully executed, Labels class can handle also when a key in the hashmap is not present.
-		// Whilst Translator verifies the user program is correct, Labels class could be used wrongly by the engineer (e.g. the wrong string variable passed to the method possibly?).
 		if (!labels.containsKey(label)){
 			throw new NullPointerException("Label '" + label + "' does not have an associated address");
 		}
@@ -50,8 +48,7 @@ public final class Labels {
 	}
 
 	/**
-	 * representation of this instance,
-	 * in the form "[label -> address, label -> address, ..., label -> address]"
+	 * Returns a string representation of this instance in the form "[label -> address, label -> address, ..., label -> address]".
 	 *
 	 * @return the string representation of the labels map
 	 */
@@ -63,6 +60,12 @@ public final class Labels {
 				.collect(Collectors.joining(", ", "[", "]"));
 	}
 
+	/**
+	 * Indicates whether some other object is "equal to" this one.
+	 *
+	 * @param o the object to compare
+	 * @return true if this object is the same as the o argument; false otherwise
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
@@ -70,6 +73,11 @@ public final class Labels {
 		return labels.equals(that.labels);
 	}
 
+	/**
+	 * Returns a hash code value for the object.
+	 *
+	 * @return a hash code value for this object
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(labels);
