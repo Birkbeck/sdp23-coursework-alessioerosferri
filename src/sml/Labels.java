3c165634a65b7815c6284f5859e85947f2f7a934
Alessio Ferri <alessio.ferri@thoughtworks.com>
2023-02-25 13:17:45 +0000

    Completed TODOs for Labels and implemented tests

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -3,12 +3,13 @@ package sml;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-
-// TODO: write a JavaDoc for the class
+import java.util.stream.Collectors;
 
 /**
+ * This class contains the labels used by the program. It allows the machine to reference instruction addresses by label (if a label is used).
+ * It ensures uniqueness of labels in the SML program.
  *
- * @author ...
+ * @author alessioerosferri
  */
 public final class Labels {
 	private final Map<String, Integer> labels = new HashMap<>();
@@ -18,23 +19,33 @@ public final class Labels {
 	 *
 	 * @param label the label
 	 * @param address the address the label refers to
+	 * @throws IllegalArgumentException if label already existing.
 	 */
 	public void addLabel(String label, int address) {
 		Objects.requireNonNull(label);
-		// TODO: Add a check that there are no label duplicates.
+		if (labels.containsKey(label)){
+			throw new IllegalArgumentException("Label '" + label + "' is already defined in the program");
+		}
+
 		labels.put(label, address);
 	}
 
 	/**
-	 * Returns the address associated with the label.
+	 * Returns the address associated with the label. If the label passed does not exist, it will throw a NullPointerException.
 	 *
 	 * @param label the label
 	 * @return the address the label refers to
+	 * @throws NullPointerException if label already existing.
 	 */
 	public int getAddress(String label) {
-		// TODO: Where can NullPointerException be thrown here?
-		//       (Write an explanation.)
-		//       Add code to deal with non-existent labels.
+		// Where can NullPointerException be thrown here?
+		// Checking whether a label exists is something that could happen either in here or in the Translator class. Because both Translator and Labels class are public, and therefore could be used by any user (engineer)
+		// I am making a design decision to check non-existent labels in both places. Translator ensures the program text passed in can be successfully executed, Labels class can handle also when a key in the hashmap is not present.
+		// Whilst Translator verifies the user program is correct, Labels class could be used wrongly by the engineer (e.g. the wrong string variable passed to the method possibly?).
+		if (!labels.containsKey(label)){
+			throw new NullPointerException("Label '" + label + "' does not have an associated address");
+		}
+
 		return labels.get(label);
 	}
 
@@ -46,11 +57,23 @@ public final class Labels {
 	 */
 	@Override
 	public String toString() {
-		// TODO: Implement the method using the Stream API (see also class Registers).
-		return "";
+		return labels.entrySet()
+				.stream()
+				.map(entry -> entry.getKey() + " -> " + entry.getValue())
+				.collect(Collectors.joining(", ", "[", "]"));
 	}
 
-	// TODO: Implement equals and hashCode (needed in class Machine).
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Labels labels1)) return false;
+		return labels.equals(labels1.labels);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(labels);
+	}
 
 	/**
 	 * Removes the labels
---
+++ test/sml/LabelsTest.java
@@ -0,0 +1,73 @@
+package sml;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabelsTest {
+    private Labels labels;
+
+    @BeforeEach
+    void setUp() {
+        labels = new Labels();
+    }
+
+    @AfterEach
+    void tearDown() {
+        labels = null;
+    }
+
+    @Test
+    void addLabelSuccess() {
+        labels.addLabel("L", 1);
+        Assertions.assertEquals("[L -> 1]", labels.toString());
+    }
+
+    @Test
+    void addLabelDuplicate() {
+        labels.addLabel("L", 1);
+        Assertions.assertThrows(IllegalArgumentException.class, ()->labels.addLabel("L", 2));
+    }
+
+    @Test
+    void getAddressSuccess() {
+        labels.addLabel("L", 1);
+        Assertions.assertEquals(1, labels.getAddress("L"));
+    }
+
+    @Test
+    void getAddressNonExistent() {
+        Assertions.assertThrows(NullPointerException.class, ()->labels.getAddress("L"));
+    }
+
+    @Test
+    void testToString() {
+        labels.addLabel("L", 1);
+        labels.addLabel("K", 1);
+        Assertions.assertEquals("[K -> 1, L -> 1]", labels.toString());
+    }
+
+    @Test
+    void testEquals() {
+        Assertions.assertEquals(labels, new Labels());
+        labels.addLabel("k", 1);
+        Assertions.assertNotEquals(labels, new Labels());
+    }
+
+    @Test
+    void testHashCode() {
+        Assertions.assertEquals(labels.hashCode(), new Labels().hashCode());
+        labels.addLabel("k", 1);
+        Assertions.assertNotEquals(labels.hashCode(), new Labels().hashCode());
+    }
+
+    @Test
+    void reset() {
+        labels.addLabel("L", 1);
+        labels.reset();
+        Assertions.assertEquals("[]", labels.toString());
+    }
+}
