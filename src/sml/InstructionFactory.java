7fb21f823e1e460bfec2182b9230362e2d082261
Alessio Ferri <alessio.ferri@thoughtworks.com>
2023-02-26 22:02:47 +0000

    Added javadoc

--- src/sml/InstructionFactory.java
+++ src/sml/InstructionFactory.java
@@ -9,15 +9,39 @@ import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+/**
+ * The InstructionFactory class is responsible for creating Instruction objects based on the opcode given.
+ * It uses Spring Framework's BeanFactory to create the object and pass the required parameters to its constructor.
+ *
+ * @author alessioerosferri
+ */
 public class InstructionFactory {
     private static final InstructionFactory instance = new InstructionFactory();
     private BeanFactory beanFactory;
-    public static InstructionFactory getInstance() {
-        return instance;
-    }
     private InstructionFactory(){
         beanFactory = new ClassPathXmlApplicationContext("/beans.xml");
     }
+
+    /**
+     * Returns the singleton instance of the InstructionFactory.
+     *
+     * @return the instance.
+     */
+    public static InstructionFactory getInstance() {
+        return instance;
+    }
+
+    /**
+     * Creates an Instruction object based on the opcode given.
+     * Uses reflection to determine the class of the Instruction and its required parameters.
+     * The parameters are obtained by calling the Supplier function passed as an argument.
+     * Returns null if the class cannot be instantiated.
+     *
+     * @param label the label for the Instruction.
+     * @param opcode the opcode for the Instruction.
+     * @param scan the Supplier function used to obtain the parameters for the Instruction.
+     * @return the Instruction object created or null if the class cannot be instantiated.
+     */
     public Instruction createInstruction(String label, String opcode, Supplier<String> scan){
         try {
             Class<?> klass = beanFactory.getType(opcode);
